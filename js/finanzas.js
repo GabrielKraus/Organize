3bac1d52320604b3bf557b40b2b0f98d51d8fe39
GabrielKraus <gabrielalvarezkraus@gmail.com>
2022-03-07 21:25:11 -0300

    first commit

--- js/finanzas.js
+++ js/finanzas.js
@@ -1,3 +1,14 @@
+
+fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
+    .then((resp) => { return resp.json() })
+    .then((data) => {
+        let dolarBlueObj = Object.values(data[1])[0]
+        let valorDelDolar = parseInt(dolarBlueObj.compra)
+        agregarItemLocalStorage("valorDolar", valorDelDolar)
+
+    }).catch()
+
+
 function agregarItemAlDom(nuevoElemento, elementoDestino, valorAMostrar) {
     display = document.querySelector(elementoDestino)
     let valorPantalla = document.createElement(`${nuevoElemento}`)
@@ -32,15 +43,6 @@ function agregarSueldo() {
 agregarItemAlDom("h2", "#sueldoDisplay", `$${rescatarItemDelStorage("sueldo")}`)
 
 
-
-
-
-
-
-
-
-
-
 let gastoGananciaAgregar = document.getElementById("gastoGananciaAgregar").addEventListener("click", AgregarGastoOGanancia)
 
 let conjuntoDeGastos = [];
@@ -51,17 +53,17 @@ function AgregarGastoOGanancia() {
     let divisaSelect = document.getElementById('divisaSelect');
     let divisaSelectValor = divisaSelect.options[divisaSelect.selectedIndex].value;
 
-    divisaSelectValor == "dolares" ? gastoGananciaValorNumeral = (gastoGananciaValorNumeral*207) : console.log("la moneda permanesera como pesos Argentinos")
+    divisaSelectValor == "dolares" ? gastoGananciaValorNumeral = (gastoGananciaValorNumeral * parseFloat(rescatarItemDelStorage("valorDolar"))) : console.log("la moneda permanecera como pesos Argentinos")
 
     gastoGananciaSelectValor == "gasto" ? gastoGananciaValorNumeral = -Math.abs(gastoGananciaValorNumeral) : console.log("el monto sera considerado una ganancia")
-    
+
     conjuntoDeGastos.push(gastoGananciaValorNumeral)
     agregarItemLocalStorage("gastos", conjuntoDeGastos)
 }
 
 let botonBorrarGastos = document.getElementById("borrarGastos").addEventListener("click", borrarGastos)
 
-function borrarGastos(){
+function borrarGastos() {
     localStorage.removeItem("gastos");
 }
 
@@ -71,19 +73,21 @@ function borrarGastos(){
 
 let gastosStorage = rescatarItemDelStorage("gastos")
 
-for(i=0; i<gastosStorage.length; i++){
+for (i = 0; i < gastosStorage.length; i++) {
     conjuntoDeGastos.push(rescatarItemDelStorage("gastos")[i])
     agregarItemAlDom("h4", "#gastosYGananciasDisplay", `$${rescatarItemDelStorage("gastos")[i]}`);
 }
 
 let total = parseFloat(rescatarItemDelStorage("sueldo"))
-for(i=0; i<gastosStorage.length; i++){
-    
+for (i = 0; i < gastosStorage.length; i++) {
+
 
     total += rescatarItemDelStorage("gastos")[i]
 }
 
 agregarItemAlDom("h4", "#montoTotal", `$${total}`);
+
+
 
 
 
